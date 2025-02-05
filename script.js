64b201d32368ae345f19e4582dcee8a6351ebab2
Rishu Kumar <126085066+rishu-7549@users.noreply.github.com>
2025-02-05 17:08:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-//your JS code here. If required.
-let st = document.getElementById("status");
-let btn = document.getElementById("enterBtn");
+// //your JS code here. If required.
+// let st = document.getElementById("status");
+// let btn = document.getElementById("enterBtn");
 
 
 
-btn.addEventListener('click', function () {
+// btn.addEventListener('click', function () {
 
-  let heading = document.createElement("h1");
-  heading.innerHTML = "Entered Metaverse";
-  st.parentNode.replaceChild(heading, st);
-})
+//   let heading = document.createElement("h1");
+//   heading.innerHTML = "Entered Metaverse";
+//   st.parentNode.replaceChild(heading, st);
+// });
+
+document.getElementById("enterBtn").addEventListener("click", function() {
+let paragraph = document.getElementById("status");
+
+let heading = document.createElement("h1");
+heading.innerHTML = "Entered Metaverse"; // Set new text
+
+paragraph.parentNode.replaceChild(heading, paragraph);
+});
