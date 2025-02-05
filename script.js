dd11defaecd023ff48c342e7b32639897adc7d7e
Rishu Kumar <126085066+rishu-7549@users.noreply.github.com>
2025-02-05 16:46:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let st = document.getElementById("status");
+let btn = document.getElementById("enterBtn");
+
+
+
+btn.addEventListener('click', function () {
+
+  let heading = document.createElement("h1");
+  heading.innerHTML = "Entered Metaverse";
+  st.parentNode.replaceChild(heading, st);
+})
